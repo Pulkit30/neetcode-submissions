388ac6a560ed0f1d9463a1d321437c8e5663b739
Pulkit Sharma <pulkitsharma1816@gmail.com>
2026-09-15 12:54:26 +0530

    Add: sql-not-null-constraint - submission-3

---
+++ SQL/sql-not-null-constraint/submission-3.sql
@@ -0,0 +1,12 @@
+
+
+create table products(
+    name text NOT NULL DEFAULT 'Unknown',
+    price Integer NOT NULL,
+    quantity integer default 0);
+
+
+-- Do not modify below this line --
+SELECT column_name, data_type, is_nullable, column_default
+FROM information_schema.columns
+WHERE table_name = 'products';
